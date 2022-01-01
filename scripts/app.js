504e7218b5a13c432b4fec7ffd0ad4dbc783d1bc
Nathan Sebhastian <nathansebhastian@gmail.com>
2022-01-01 17:40:09 +0700

    fix app.js content not firing

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  requirejs(["lodash"], function (lodash) {
-    const headerEl = document.getElementById("header");
-    headerEl.textContent = lodash.upperCase("hello world");
-  });
-});
+requirejs(['lodash'], function (lodash) {
+  const headerEl = document.getElementById('header')
+  headerEl.textContent = lodash.upperCase('hello world')
+})
